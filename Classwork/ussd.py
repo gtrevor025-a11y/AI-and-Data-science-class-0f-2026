47b74b6ed3f21cc764eecf417bb8dc1144717c75
gtrevor025-a11y <gtrevor025@gmail.com>
2026-02-07 11:54:55 +0300

    Add files via upload
    A simple choice selector for ussd will continually improve

---
+++ Classwork/ussd.py
@@ -0,0 +1,19 @@
+print("welcome to my ussd ")
+print("1.Buy airtime")
+print("2.Okoa airtime")
+print("3.Lipa okoa ")
+print("4.Tunukiwa")
+print("5.Data offers")
+choice=int(input("Enter a valid number to choose : "))
+if choice==1 :
+	print("Buy airtime : ")
+elif choice==2 :
+	print("welcome to okoa airtime")
+elif choice ==3:
+	print("Lipa okoa ")
+elif choice==4 :
+	print("Tunukiwa ")
+elif choice==5 :
+	print("Data offers")
+else :
+	print("invalid choice ")
