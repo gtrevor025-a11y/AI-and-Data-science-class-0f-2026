a14db3b82bbfe7e168d36ab22d49848eb0aa1147
gtrevor025-a11y <gtrevor025@gmail.com>
2026-02-07 09:10:58 +0300

    Add files via upload

---
+++ assignments/input names.py
@@ -0,0 +1,9 @@
+Name=input("what is your name ? ")
+DOB=input("what is your date of birth ? ")
+DOB=int(DOB)
+age=2026-DOB
+fruit=input("what is your favorite fruit ? ")
+print("welcome " + Name + " to your first input program")
+print("you are ",age,"Years old")
+print("His favorite fruit is " + fruit)
+print(f"conglatulations {Name} You are {age} years old")
